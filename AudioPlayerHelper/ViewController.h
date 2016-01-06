27ada2ac1d9668fb1c186f263f2f8c6e27ac7194
CK-chan <692771080@qq.com>
2016-01-06 18:26:48 +0800

    Initial Commit

---
+++ AudioPlayerHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioPlayerHelper
+//
+//  Created by 番茄说 on 16/1/6.
+//  Copyright © 2016年 CK_chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AudioPlayerHelper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AudioPlayerHelper
+//
+//  Created by 番茄说 on 16/1/6.
+//  Copyright © 2016年 CK_chan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
